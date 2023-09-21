67800e2ac97541e0b02d476454d2440717148253
faizanpasha356 <mdalifaizan356@gmail.com>
2023-09-22 02:20:38 +0530

    Update file

--- CC++andDS/Array.cpp
+++ CC++andDS/Array.cpp
@@ -1,8 +1,14 @@
+/* 
+    Array is a linear collection of similar elements
+    Array is also known as subscript variabla
+    Array is a group of variable
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int num [10];
+    cout<<num[0];
     
-
-
     return 0;
 }
